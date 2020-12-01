da781244e1cad168ca0f8aedfa5234218bf4c0b8
daniele <d.malitesta@gmail.com>
2020-12-01 21:06:07 +0100

    Train/validation/test splitting optimized and tested (leave-one-out).

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -7,9 +7,7 @@ random.seed(1234)
 
 parser = argparse.ArgumentParser(description="Run dataset splitting.")
 parser.add_argument('--dataset', nargs='?', default='amazon_clothing', help='dataset name')
-parser.add_argument('--test_size', type=float, default=0.2, help='test set size')
-parser.add_argument('--percentage', type=bool, default=False, help='whether using a % to express val and test set')
-parser.add_argument('--validation_size', type=float, default=0.1, help='validation set size')
+parser.add_argument('--validation', type=bool, default=False, help='True --> use validation, False --> no validation')
 parser.add_argument('--shuffle', type=bool, default=True, help='dataset shuffle')
 parser.add_argument('--column_stratify', type=list, default=[0], help='list of columns to use for stratification')
 args = parser.parse_args()
@@ -19,41 +17,22 @@ df = pd.read_csv(all_interactions.format(args.dataset), delimiter='\t', header=N
 
 num_users, num_items = df[0].nunique(), df[1].nunique()
 
-interactions = df.copy()
-interactions = interactions.sample(len(interactions)).reset_index(drop=True)
+df = df.groupby(args.column_stratify).apply(lambda x: x.sample(frac=1)).reset_index(drop=True)
+df_grouped = df.groupby(args.column_stratify)
 
-validation, test = [], []
+if args.validation:
+    validation = df_grouped.tail(1).drop_duplicates()
+    validation[2] = [1.0] * len(validation)
+    validation.to_csv(validation_path.format(args.dataset), index=False, sep='\t', header=None)
+    df.drop(index=validation.index, inplace=True)
 
-for u in range(num_users):
-    current_item = interactions[interactions[0] == u].iloc[0, 1]
-    index = interactions[interactions[0] == u].iloc[0].name
-    validation.append({
-        0: u,
-        1: current_item
-    })
-    interactions.drop(index=index, inplace=True)
+df_grouped = df.groupby(args.column_stratify)
+test = df_grouped.tail(1).drop_duplicates()
+train = df.drop(index=test.index)
 
-for u in range(num_users):
-    current_item = interactions[interactions[0] == u].iloc[0, 1]
-    index = interactions[interactions[0] == u].iloc[0].name
-    test.append({
-        0: u,
-        1: current_item
-    })
-    interactions.drop(index=index, inplace=True)
-
-validation = pd.DataFrame(validation)
-test = pd.DataFrame(test)
-
-# sort according to user
-train = interactions.sort_values(by=args.column_stratify)
-validation = validation.sort_values(by=args.column_stratify)
-test = test.sort_values(by=args.column_stratify)
 train[2] = [1.0] * len(train)
-validation[2] = [1.0] * len(validation)
 test[2] = [1.0] * len(test)
 
 # write to file
 train.to_csv(training_path.format(args.dataset), index=False, sep='\t', header=None)
-validation.to_csv(validation_path.format(args.dataset), index=False, sep='\t', header=None)
 test.to_csv(test_path.format(args.dataset), index=False, sep='\t', header=None)
