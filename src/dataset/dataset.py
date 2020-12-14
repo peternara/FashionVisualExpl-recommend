e179f980ef39b0875879255f8ecfe209bee5010e
daniele <d.malitesta@gmail.com>
2020-12-14 10:13:18 +0100

    CompVBPR completed and tested.

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -52,7 +52,10 @@ class DataLoader(object):
         # Get number of users and items
         u_ = 0
         items = []
-        with open(self.path_train_data, "r") as f:
+        read_path = self.path_train_data if train_val_test == 'train' \
+            else self.path_validation_data if train_val_test == 'val' \
+            else self.path_test_data
+        with open(read_path, "r") as f:
             line = f.readline()
             index = 0
             while line is not None and line != "":
